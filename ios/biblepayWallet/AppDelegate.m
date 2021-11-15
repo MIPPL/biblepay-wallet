0b375e583a7f3c8158d9222340f59a1a07bb1087
MIPPL <>
2021-11-15 14:24:49 +0100

    [iOS] remove unimodules

--- ios/biblepayWallet/AppDelegate.m
+++ ios/biblepayWallet/AppDelegate.m
@@ -11,31 +11,15 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
-#import <UMCore/UMModuleRegistry.h>
-#import <UMReactNativeAdapter/UMNativeModulesProxy.h>
-#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
-
-@interface AppDelegate () <RCTBridgeDelegate>
-@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
-@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
-  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   [ReactNativeNavigation bootstrap:[self sourceURLForBridge: bridge] launchOptions:launchOptions];
   
-  [super application:application didFinishLaunchingWithOptions:launchOptions];
   return YES;
-}
-- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
-{
-      NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge];
-      // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
-      return extraModules;
 }
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
